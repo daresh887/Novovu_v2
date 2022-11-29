b213d7790a9995bbf69207a47698239c9f2ca1b1
MrMixerr <52745857+MrMixerr@users.noreply.github.com>
2022-11-29 17:46:30 +0200

    made working check if logged in system

--- index.js
+++ index.js
@@ -2,14 +2,37 @@ import Express from "express";
 const port = 3000;
 const app = Express();
 
+var loggedin = false;
 
 app.set('view engine', 'ejs');
 app.use("/assets",Express.static('assets'));
 
 
+app.post("/register", function(req, res){
+    // this should stay here for now
+    // change later 
+    loggedin = true
+    res.redirect("/")
+    
+})
+
+app.get("/register", function(req, res){
+    if (!loggedin == true) {
+        res.render("register", {user:"TestingDan"})
+    }
+    else{
+        res.redirect("/home")
+    }
+})
 
 app.get("/", function(req, res){
-    res.render("home", {user:"TestingDan"})
+    // change later
+    if (loggedin == true) {
+        res.render("home", {user:"TestingDan"})
+    }
+    else{
+        res.redirect("/register")
+    }
 })
 
 
